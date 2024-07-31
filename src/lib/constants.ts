5e6745a98001038a0ff2ce1bf069aaf8d64ac163
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-07-31 22:41:59 +0700

    feat: created constants for the filters

---
+++ src/lib/constants.ts
@@ -0,0 +1,23 @@
+// Subscription Categories
+export const SUBSCRIPTION_CATEGORIES = [
+  'Entertainment',
+  'Work',
+  'Home',
+  'Games',
+  'Education',
+  'Health',
+  'Others'
+];
+
+// Subscription Price Ranges
+export const SUBSCRIPTION_PRICE_RANGES = [
+  'Less than Rp100.000',
+  'Rp100.000 - Rp200.000',
+  'Rp200.000 - Rp300.000',
+  'Rp300.000 - Rp400.000',
+  'Rp400.000 - Rp500.000',
+  'Rp500.000 and above'
+];
+
+// Subscription Status
+export const SUBSCRIPTION_STATUS = ['Inactive', 'Upcoming', 'Active', 'Overdue'];
