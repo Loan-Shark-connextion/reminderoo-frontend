bb3470936514c3a48a1abc28885a313184e8fcaa
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-08-02 01:15:10 +0700

    feat: revamped filter dropdowns to work with data table and the updated constants

--- src/components/parts/FilterDropdown/index.tsx
+++ src/components/parts/FilterDropdown/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   Select,
   SelectContent,
@@ -6,23 +8,39 @@ import {
   SelectValue
 } from '@/components/ui/select';
 
-const FilterDropdown = ({ name, data }: { name: string; data: string[] }) => {
+/* eslint-disable no-unused-vars  */
+export type FilterDropdownType = {
+  filterFn: (selectedValue: string, title: string) => void;
+  title: string;
+  data: string[] | Record<string, number[]>;
+};
+/* eslint-enable no-unused-vars  */
+
+const FilterDropdown = ({ filterFn, title, data }: FilterDropdownType) => {
   return (
     <div className="w-1/3 flex flex-col gap-2">
-      <p className="text-body-sm font-medium text-primary-80 capitalize">{name}</p>
-      <Select>
+      <p className="text-body-sm font-medium text-primary-80 capitalize">{title}</p>
+      <Select onValueChange={(value) => filterFn(value, title)}>
         <SelectTrigger className="bg-muted">
-          <SelectValue placeholder={`All ${name}`} className="capitalize" />
+          <SelectValue placeholder={`All ${title}`} className="capitalize" />
         </SelectTrigger>
         <SelectContent>
           <SelectItem value="all" className="capitalize">
-            All {name}
+            All {title}
           </SelectItem>
-          {data.map((field: string) => (
-            <SelectItem key={field} value={field}>
-              {field}
-            </SelectItem>
-          ))}
+          {Array.isArray(data)
+            ? data.map((field: string) => (
+                // eslint-disable-next-line react/jsx-indent
+                <SelectItem key={field} value={field}>
+                  {field}
+                </SelectItem>
+              ))
+            : Object.keys(data).map((field: string) => (
+                // eslint-disable-next-line react/jsx-indent
+                <SelectItem key={field} value={field}>
+                  {field}
+                </SelectItem>
+              ))}
         </SelectContent>
       </Select>
     </div>
