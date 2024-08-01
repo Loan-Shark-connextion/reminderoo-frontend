38860df0c49a8b4a1504598c435c13b95b6b3c79
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-08-02 01:00:52 +0700

    Revert "feat: changed the format of subscription price range constant"

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -9,19 +9,15 @@ export const SUBSCRIPTION_CATEGORIES = [
   'Others'
 ];
 
-export interface SubscriptionPriceRanges {
-  [key: string]: number[];
-}
-
 // Subscription Price Ranges
-export const SUBSCRIPTION_PRICE_RANGES: SubscriptionPriceRanges = {
-  'Less than Rp.100.000': [0, 100],
-  'Rp.100.000 - Rp.200.000': [100, 200],
-  'Rp.200.000 - Rp.300.000': [200, 300],
-  'Rp.300.000 - Rp.400.000': [300, 400],
-  'Rp.400.000 - Rp.500.000': [400, 500],
-  'Rp.500.000 and above': [500, Infinity]
-};
+export const SUBSCRIPTION_PRICE_RANGES = [
+  'Less than Rp100.000',
+  'Rp100.000 - Rp200.000',
+  'Rp200.000 - Rp300.000',
+  'Rp300.000 - Rp400.000',
+  'Rp400.000 - Rp500.000',
+  'Rp500.000 and above'
+];
 
 // Subscription Status
 export const SUBSCRIPTION_STATUS = ['Inactive', 'Upcoming', 'Active', 'Overdue'];
