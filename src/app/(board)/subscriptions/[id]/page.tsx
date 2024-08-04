a0067202d8f742c90e924306ec78be04c815aa76
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-08-04 23:51:49 +0700

    feat: slicing subscription detail

--- src/app/(board)/subscriptions/[id]/page.tsx
+++ src/app/(board)/subscriptions/[id]/page.tsx
@@ -1,13 +1,9 @@
+import SubscriptionDetail from '@/components/parts/SubscriptionDetail';
+
 export default function SubscriptionDetailPage() {
   return (
     <>
-      <section className="bg-emerald-300 lg:col-span-8">
-        <article className="flex flex-col">
-          <section>
-            <h1 className="text-3xl">Detail Subscription</h1>
-          </section>
-        </article>
-      </section>
+      <SubscriptionDetail />
       <section className="bg-violet-400 lg:col-span-4">
         <article className="flex flex-col">
           <h1 className="text-3xl">Payment Subscription</h1>
