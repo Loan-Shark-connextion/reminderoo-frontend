0f62e5973ae43f74cb328230e1d75b8c28760972
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-08-05 16:07:11 +0700

    feat: moved details page into it's own file

--- src/app/(board)/subscriptions/[id]/page.tsx
+++ src/app/(board)/subscriptions/[id]/page.tsx
@@ -1,14 +1,5 @@
-import SubscriptionDetail from '@/components/parts/SubscriptionDetail';
+import DetailsPage from '@/components/pages/DetailsPage';
 
 export default function SubscriptionDetailPage() {
-  return (
-    <>
-      <SubscriptionDetail />
-      <section className="bg-violet-400 lg:col-span-4">
-        <article className="flex flex-col">
-          <h1 className="text-3xl">Payment Subscription</h1>
-        </article>
-      </section>
-    </>
-  );
+  return <DetailsPage />;
 }
